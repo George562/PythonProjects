4b5070f77cc74555da0a2964260fcdec8be4d2f1
George562 <51839473+George562@users.noreply.github.com>
2019-12-24 20:27:45 +0300

    Update 3.py

--- bruh/3.py
+++ bruh/3.py
@@ -1,12 +1,7 @@
+ans = set()
 s = int(input())
-nums = [int(input()) for _ in range(s)]
-ans = []
-for x in sorted(nums):
-    a, b = x-1 if x != 1 else 1, x+1 if x != s else s
-    if a not in ans:
-        ans.append(a)
-    elif x not in ans:
-        ans.append(x)
-    elif b not in ans:
-        ans.append(b)
-print(len(ans))
+for _ in range(s):
+    x = int(input())
+    a, b = x-1 if x != 1 else 1, x+1
+    ans |= {a, x, b}
+print(len(ans) if len(ans) < s else s)
