07f5f60813d4486fc72e686d017191c58d2a0e2c
George562 <51839473+George562@users.noreply.github.com>
2020-01-12 15:29:52 +0300

    Update fracs.py

--- utitled/fracs.py
+++ utitled/fracs.py
@@ -1,6 +1,5 @@
 import pygame as pg
 from math import hypot, cos, sin, atan2
-import os
 from time import time
 
 def zoom(do):
@@ -12,41 +11,47 @@ def zoom(do):
         for i in range(len(arr)):
             arr[i] = (arr[i][0]-x)/1.05+x, (arr[i][1]-y)/1.05+y
 
-def fractal(depth, sd=0):
+def lom(x, y, x1, y1):
+    h = hypot(x-x1, y-y1)
+    rx, ry = (x1-x), (y1-y)
+    angle = -atan2(rx, ry)
+    return [(x+rx/3, y+ry/3), ((x+x1)/2+cos(angle)*h/3, (y1+y)/2+sin(angle)*h/3), (x+rx*2/3, y+ry*2/3)]
+
+def fractal(depth):
     print(depth, time()-t)
     if depth > 0:
-        h = hypot(arr[0][0]-arr[1][0], arr[0][1]-arr[1][1])
-        for i in arr.copy()[:len(arr)-1]:
-            x, y, x1, y1 = *i, *arr[arr.index(i)+1]
-            i = arr.index(i)
-            rx, ry = (x1-x), (y1-y)
-            angle = -atan2(rx, ry)
-            arr.insert(i+1, (x+rx/3, y+ry/3))
-            arr.insert(i+2, ((x+x1)/2+cos(angle)*h/3, (y1+y)/2+sin(angle)*h/3))
-            if sd != 0:
-                arr.insert(i+3, (x+rx*2/3, y+ry*2/3))
-            else:
-                arr.pop()
-        fractal(depth-1, depth)
+        for i in range(len(arr)-1):
+            dop = lom(*arr[i*4], *arr[i*4+1])
+            for j in range(3):
+                arr.insert(i*4+j+1, dop[j])
+        dop = lom(*arr[-1], *arr[0])
+        for i in range(3):
+            arr.append(dop[i])
+        fractal(depth-1)
 
 topTime = time()
 t = time()
 scw, sch = 750, 750
 sc = pg.display.set_mode((scw, sch))
 z = 1
-d = 10
-arr = [(scw/3, sch/3), (scw*2/3, sch/3)]
+d = 5
+arr = [(scw/3, sch/3), (scw*2/3, sch/3), (scw/2, sch/3*(0.75**0.5)+sch/3)]
 fractal(d)
 print(len(arr))
 print(time()-topTime)
-a = len(arr)//(3**(d))*4
 clock = pg.time.Clock()
 done = False
 while not done:
     sc.fill((0, 0, 0))
-    for i in range(0, len(arr)-1, int(a/z)+1):
-        if 0 <= arr[i][0] <= scw and 0 <= arr[i][1] <= sch and 0 <= arr[i+1][0] <= scw and 0 <= arr[i+1][1] <= sch:
+    for i in range(len(arr)-1):
+        if 0 <= arr[i][0] <= scw and 0 <= arr[i][1] <= sch or 0 <= arr[i+1][0] <= scw and 0 <= arr[i+1][1] <= sch:
             pg.draw.line(sc, (255, 255, 255), arr[i], arr[i+1])
+            if all([0 <= arr[i][0] <= scw, 0 <= arr[i][1] <= sch, 0 <= arr[i+1][0] <= scw, 0 <= arr[i+1][1] <= sch]):
+                dh = hypot(arr[i][0]-arr[i+1][0], arr[i][1]-arr[i+1][1])
+                if dh > 20:
+                    dop = lom(*arr[i], *arr[i+1])
+                    for j in range(3):
+                        arr.insert(i+j+1, dop[j])
     if pg.mouse.get_pressed()[0]:
         zoom(True)
         z *= 1.05
@@ -57,4 +62,4 @@ while not done:
         if e.type == pg.QUIT:
             done = True
     pg.display.flip()
-    clock.tick(60)
+    clock.tick(40)
