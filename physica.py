7401fcf711ad010461717bdd9ee0bda6a623482a
George562 <51839473+George562@users.noreply.github.com>
2020-01-31 17:23:40 +0300

    Update physica.py

--- physica.py
+++ physica.py
@@ -2,6 +2,7 @@ try:
     import pygame as pg
 except ModuleNotFoundError:
     import os
+
     os.system('pip install pygame')
     import pygame as pg
 import math as m
@@ -12,12 +13,25 @@ pg.init()
 black = (0, 0, 0)
 yellow = (255, 255, 0)
 white = (255, 255, 255)
+green = (50, 255, 50)
+
+temp = 80
 
 font = (lambda size, text: pg.font.SysFont('verdana', size).render(str(text), False, white))
+real = (lambda x: x * temp / mapDX)
+
+
+def coordinates(dx):
+    for i in range((workplace[3][0] - workplace[1][0]) // temp + 1):
+        val = font(15, -int(mapX) + i * dx)
+        sc.blit(val, (workplace[1][0] + i * temp - val.get_width() / 2, workplace[3][1]))
+    for i in range((workplace[3][1] - workplace[1][1]) // temp + 1):
+        val = font(15, int(mapY) + i * dx)
+        sc.blit(val, (workplace[1][1], workplace[3][1] - i * temp - 15))
 
 
 class Obj:
-    def __init__(self, x, y, vx=0, vy=0, ax=0, ay=0):
+    def __init__(self, x=0, y=0, vx=0, vy=0, ax=0, ay=0):
         self.x = x
         self.y = y
         self.vx = vx
@@ -27,6 +41,7 @@ class Obj:
 
 
 class Slider:
+    actTyp = None
     h = 30  # height
 
     def __init__(self, win, w, x, y, value, name=''):
@@ -36,143 +51,138 @@ class Slider:
         self.y = y  # y coordinate
         self.w = w  # width
         self.value = value  # present value
-        self.name_x = self.x-self.name.get_width()-15 if name != '' else self.x
+        self.name_x = self.x - self.name.get_width() - 10 if name != '' else self.x
         self.default = (win, w, x, y, value, name)  # default parameters
 
     def draw(self):
         if self.name is not None:
             self.win.blit(self.name, [self.name_x, self.y])
-        self.win.blit(font(18, self.value), [self.x+3, self.y])
+        val = font(18, self.value)
+        self.win.blit(val, [self.x + (self.w - val.get_width()) / 2, self.y + (self.h - val.get_height()) / 2])
         pg.draw.rect(self.win, white, (self.x, self.y, self.w, self.h), 2)  # bg
 
     def update(self, x, y):
-        global actTyp, typing
-        if self.x <= x <= self.x+self.w+1 and self.y <= y <= self.y+self.h:
+        if 0 <= x - self.x <= self.w and 0 <= y - self.y <= self.h:
             if pg.mouse.get_pressed()[0]:
-                actTyp = self
-                typing = True
+                Slider.actTyp = self
                 self.value = 0
 
 
 def physic():
     global simulate
     obj.ax = obj.ay = 0
-    ea = E*obj.q/obj.mass
-    if obj.x > L + start[0]:
-        obj.ax -= ea*m.cos(m.radians(-Eangle))
-        obj.ay -= ea*m.sin(m.radians(-Eangle))
+    ea = E.value * q.value / obj.mass
+    if obj.x > L.value:
+        obj.ax -= ea * m.cos(m.radians(-Eangle.value))
+        obj.ay -= ea * m.sin(m.radians(-Eangle.value))
     obj.vx += obj.ax * DT
     obj.x += obj.vx * DT
     obj.ay += gravity
     obj.vy += obj.ay * DT
     obj.y += obj.vy * DT
-    if obj.y > start[1]:
+    if obj.y > 0:
         simulate = False
 
 
 scw, sch = 1000, 700
 sc = pg.display.set_mode((scw, sch))
 
-typing = False
-actTyp = None
-
 gravity = 9.81
-E = 100
-Eangle = 180
 
-v = 100
-angle = 45
-start = (50, 600)
-obj = Obj(*start)
-obj.q = 1
+start = (50, 560)
+obj = Obj()
 obj.mass = 1
-L = 500
 Tstart, Tend = 0, 0
 zoom = 1
 DT = 2 * 10e-3
-workplace = [(50, 600), (50, 50), (850, 50), (850, 600)]
+workplace = [(50, 560), (50, 10), (850, 10), (850, 560)]
+trajectory = []
+lastAdd = 0
 
 # sliders
-vSlider = Slider(sc, 80, 50, 620, 100, 'v')
-angleSlider = Slider(sc, 40, 220, 620, 45, 'angle')
-lSlider = Slider(sc, 80, 310, 620, 500, 'L')
-ESlider = Slider(sc, 80, 430, 620, 100, 'E')
-qSlider = Slider(sc, 80, 560, 620, 1, 'q')
-EangleSlider = Slider(sc, 80, 740, 620, 180, 'E angle')
+V = Slider(sc, 80, 50, 620, 100, 'v')
+angle = Slider(sc, 40, 220, 620, 45, 'angle')
+L = Slider(sc, 80, 310, 620, 500, 'L')
+E = Slider(sc, 80, 430, 620, 100, 'E')
+q = Slider(sc, 80, 560, 620, 1, 'q')
+Eangle = Slider(sc, 80, 740, 620, 180, 'E angle')
 
 mapX, mapY = 0, 0
+mapDX = 50
+mapDT = Slider(sc, 60, 890, 620, 30, 'rate')
 
 simulate = False
 done = False
 while not done:
     sc.fill(black)
+    coordinates(mapDX)
     mPos = pg.mouse.get_pos()
-    sc.blit(font(18, f'x = {round(obj.x-start[0], 5)}'), [860, 50])
-    sc.blit(font(18, f'y = {round(start[1]-obj.y, 5)}'), [860, 75])
-    sc.blit(font(18, f'vx = {round(obj.vx, 5)}'), [860, 100])
-    sc.blit(font(18, f'vy = {round(-obj.vy, 5)}'), [860, 125])
-    sc.blit(font(18, f'ax = {round(obj.ax, 5)}'), [860, 150])
-    sc.blit(font(18, f'ay = {round(obj.ay, 5)}'), [860, 175])
-    sc.blit(font(18, f'timer = {round(Tend-Tstart, 3)}'), [860, 200])
+    sc.blit(font(18, f'x = {round(obj.x, 4)}'), [860, 50])
+    sc.blit(font(18, f'y = {round(-obj.y, 4)}'), [860, 75])
+    sc.blit(font(18, f'vx = {round(obj.vx, 4)}'), [860, 100])
+    sc.blit(font(18, f'vy = {round(-obj.vy, 4)}'), [860, 125])
+    sc.blit(font(18, f'ax = {round(obj.ax, 4)}'), [860, 150])
+    sc.blit(font(18, f'ay = {round(obj.ay, 4)}'), [860, 175])
+    sc.blit(font(18, f'timer = {round(Tend - Tstart, 3)}'), [860, 200])
     if simulate:
         physic()
+        if time() - lastAdd > 1 / (mapDT.value + 1):
+            trajectory.append((obj.x, obj.y))
+            lastAdd = time()
         Tend = time()
-    pg.draw.circle(sc, white, (round(obj.x)+mapX, round(obj.y)+mapY), 10)
+        if not - 3 < real(obj.x + mapX) < workplace[3][0] - start[0] + 3:
+            mapX += -mapDX if workplace[3][0] - start[0] < real(obj.x + mapX) else mapDX
+        if not workplace[1][1] - 3 < real(obj.y + mapY) + start[1] < workplace[3][1] + 3:
+            mapY += -mapDX if workplace[3][1] + 3 < real(obj.y + mapY) + start[1] else mapDX
+    pg.draw.circle(sc, white, (round(real(obj.x + mapX)) + start[0], round(real(obj.y + mapY)) + start[1]), 10)
     pg.draw.polygon(sc, white, workplace, 1)
-    pg.draw.line(sc, yellow, (L+start[0]+mapX, 50), (L+start[0]+mapX, 600))
+    pg.draw.line(sc, yellow, (real(L.value + mapX) + start[0], 10), (real(L.value + mapX) + start[0], 560))
 
-    pg.draw.aaline(sc, yellow, (700, 300), (700-m.cos(m.radians(-Eangle+45))*20, 300-m.sin(m.radians(-Eangle+45))*20), 2)
-    pg.draw.aaline(sc, yellow, (700, 300), (700-m.cos(m.radians(-Eangle))*50, 300-m.sin(m.radians(-Eangle))*50), 2)
-    pg.draw.aaline(sc, yellow, (700, 300), (700-m.cos(m.radians(-Eangle-45))*20, 300-m.sin(m.radians(-Eangle-45))*20), 2)
+    pg.draw.aaline(sc, yellow, (700, 300),
+                   (700 - m.cos(m.radians(-Eangle.value + 45)) * 20, 300 - m.sin(m.radians(-Eangle.value + 45)) * 20))
+    pg.draw.aaline(sc, yellow, (700, 300),
+                   (700 - m.cos(m.radians(-Eangle.value)) * 50, 300 - m.sin(m.radians(-Eangle.value)) * 50))
+    pg.draw.aaline(sc, yellow, (700, 300),
+                   (700 - m.cos(m.radians(-Eangle.value - 45)) * 20, 300 - m.sin(m.radians(-Eangle.value - 45)) * 20))
 
-    vSlider.update(*mPos)
-    angleSlider.update(*mPos)
-    lSlider.update(*mPos)
-    ESlider.update(*mPos)
-    qSlider.update(*mPos)
-    EangleSlider.update(*mPos)
+    for slider in V, E, angle, L, q, Eangle, mapDT:
+        slider.update(*mPos)
+        slider.draw()
 
-    vSlider.draw()
-    ESlider.draw()
-    angleSlider.draw()
-    lSlider.draw()
-    qSlider.draw()
-    EangleSlider.draw()
+    for point in range(len(trajectory) - 1):
+        first = real(trajectory[point][0] + mapX) + start[0], real(trajectory[point][1] + mapY) + start[1]
+        second = real(trajectory[point + 1][0] + mapX) + start[0], real(trajectory[point + 1][1] + mapY) + start[1]
+        pg.draw.line(sc, green, first, second)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = True
         elif event.type == pg.KEYDOWN:
-            if typing:
+            if Slider.actTyp:
                 if event.key in range(48, 58):
-                    actTyp.value = int(f'{actTyp.value}{event.key - 48}')
+                    Slider.actTyp.value = int(f'{Slider.actTyp.value}{event.key - 48}')
                 elif event.key == pg.K_BACKSPACE:
-                    actTyp.value = int(str(actTyp.value)[:-1] or 0)
+                    Slider.actTyp.value = int(str(Slider.actTyp.value)[:-1] or 0)
             if event.key == pg.K_RETURN:
-                typing = False
-                angle = angleSlider.value
-                v = vSlider.value
-                L = lSlider.value
-                E = ESlider.value
-                obj.q = qSlider.value
-                Eangle = EangleSlider.value
+                Slider.actTyp = None
             if event.key == pg.K_SPACE:
-                if (obj.x, obj.y) != start:
-                    obj.x, obj.y = start
-                    obj.vx = obj.vy = obj.ax = obj.ay = 0
+                if (obj.x, obj.y) != (0, 0):
+                    obj.x = obj.y = obj.vx = obj.vy = obj.ax = obj.ay = mapX = mapY = 0
                     simulate = False
-                    mapX = mapY = 0
+                    trajectory.clear()
                 else:
                     simulate = True
                     Tstart = time()
-                    obj.vx = m.cos(m.radians(angle)) * v
-                    obj.vy = - m.sin(m.radians(angle)) * v
-                    timer = time()
-            if event.key in range(pg.K_UP, pg.K_LEFT+1):
+                    obj.vx = m.cos(m.radians(angle.value)) * V.value
+                    obj.vy = - m.sin(m.radians(angle.value)) * V.value
+            if event.key in range(pg.K_UP, pg.K_LEFT + 1):
                 if event.key in (273, 274):  # 273 Up / 274 Down
-                    mapY += 50 * (1 - 2 * (274 - event.key))
+                    mapY -= mapDX * (1 - 2 * (274 - event.key))
                 if event.key in (275, 276):  # 275 RIGHT / 276 LEFT
-                    mapX += 50 * (- 1 + 2 * (276 - event.key))
+                    mapX -= mapDX * (- 1 + 2 * (276 - event.key))
+            if event.key == pg.K_d or event.key == pg.K_u:
+                mapX, mapY = mapX // mapDX, mapY // mapDX
+                mapDX += 50 if event.key == pg.K_u else -50 if mapDX != 50 else 0
+                mapX, mapY = mapX * mapDX, mapY * mapDX
     pg.display.update()
-
 pg.quit()
