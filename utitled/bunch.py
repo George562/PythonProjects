0089ca669e0fafb2cfbb360639202046c1f3c710
George562 <51839473+George562@users.noreply.github.com>
2020-02-02 18:30:36 +0300

    Update bunch.py

--- utitled/bunch.py
+++ utitled/bunch.py
@@ -29,10 +29,12 @@ class Element:
 
 
 class Thread:
-    def __init__(self, x, y, num, k=200):
+    choice = None
+
+    def __init__(self, x, y, num, k=200, dX=4):
         self.num = num
         self.k = k
-        self.dX = 4
+        self.dX = dX
         r = num * self.dX / (m.pi * 2)
         self.els = []
         for i in range(num):
@@ -40,24 +42,25 @@ class Thread:
 
     def update(self, x, y, q, r):
         for i in range(self.num):
-            self.els[i].ax = 0
-            self.els[i].ay = 0
+            self.els[i].ax = - self.els[i].vx * GAMMA
+            self.els[i].ay = - self.els[i].vy * GAMMA
+            for j in mousePoints:
+                dx, dy = j[0] - self.els[i].x, j[1] - self.els[i].y
+                h = m.hypot(dx, dy)
+                if h > r:
+                    self.els[i].ax -= q * K / (h ** 2) * dx / h
+                    self.els[i].ay -= q * K / (h ** 2) * dy / h
+                pg.draw.circle(sc, (255, 255, 255), j, mouseR, 1)
             dx, dy = x - self.els[i].x, y - self.els[i].y
             h = m.hypot(dx, dy)
             if h > r:
                 self.els[i].ax -= q * K / (h ** 2) * dx / h
                 self.els[i].ay -= q * K / (h ** 2) * dy / h
-            elif h < self.dX + 2 and pg.mouse.get_pressed()[0]:
-                self.els[i].x = x
-                self.els[i].y = y
             for j in range(self.num):
                 if i != j:
                     set_acceleration(self.els[i], self.els[j], 0, 0, self.els[j].q)
             set_acceleration(self.els[i], self.els[(i + 1) % self.num], self.k, self.dX, 0)
             set_acceleration(self.els[i], self.els[i - 1], self.k, self.dX, 0)
-
-            self.els[i].ax -= self.els[i].vx * GAMMA
-            self.els[i].ay -= self.els[i].vy * GAMMA
 
             self.els[i].vx += self.els[i].ax * DT
             self.els[i].vy += self.els[i].ay * DT
@@ -65,24 +68,32 @@ class Thread:
             if not self.els[i].block:
                 self.els[i].x += self.els[i].vx * DT
                 self.els[i].y += self.els[i].vy * DT
+            if pg.mouse.get_pressed()[0]:
+                if h < self.dX + 3 and not self.choice:
+                    self.choice = self.els[i]
+                if self.choice:
+                    self.choice.x, self.choice.y = x, y
+            else:
+                Thread.choice = None
+
+            color = (255, 255, 0) if self.els[i].block else (255, 255, 255)
+            pg.draw.circle(sc, color, (round(self.els[i].x), round(self.els[i].y)), 2)
 
 
-scw, sch = 800, 600
+scw, sch = 900, 700
 sc = pg.display.set_mode((scw, sch))
 
 thread = Thread(scw / 2, sch / 2, 50)
-mouseQ = 10e-7
-mouseR = 20
+mouseQ = 10e-5
+mouseR = 12
+mousePoints = []
 
 done = False
 while not done:
     sc.fill((0, 0, 0))
     m_pos = pg.mouse.get_pos()
     thread.update(*m_pos, mouseQ, mouseR)
-    for element in thread.els:
-        color = (255, 255, 0) if element.block else (255, 255, 255)
-        pg.draw.circle(sc, color, (round(element.x), round(element.y)), 2)
-    pg.draw.circle(sc, (255, 255, 255), (m_pos[0], m_pos[1]), mouseR, 1)
+    pg.draw.circle(sc, (255, 255, 255), m_pos, mouseR, 1)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = True
@@ -96,4 +107,7 @@ while not done:
                     if m.hypot(element.x - m_pos[0], element.y - m_pos[1]) < thread.dX + 2:
                         element.block = not element.block
                         break
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 3:
+                mousePoints.append(m_pos)
     pg.display.update()
